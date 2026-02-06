47264a3396643bb2a3b424da8e6cf9f3db27f8f8
Mithun Mohanan <mithunmeluham@outlook.com>
2026-02-07 00:11:08 +0530

    rose day

--- src/main/java/com/meluham/ems/controller/Controller.java
+++ src/main/java/com/meluham/ems/controller/Controller.java
@@ -1,13 +1,14 @@
 package com.meluham.ems.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/ms")
 public class Controller {
+    @GetMapping("/ms")
     public String loveyou(){
-        return "<h1>I Love You Sruthi</h1>";
+        return "<h1>I Love You Sruthi</h1><br><h1>Happy ROSE DAY</h1>";
     }
     
 }
