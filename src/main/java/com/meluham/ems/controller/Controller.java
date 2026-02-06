4847779fa4d008ba40ec10d3490069e990848abc
sm <sruthipa2002@gmail.com>
2026-02-07 00:39:49 +0530

    added h1

--- src/main/java/com/meluham/ems/controller/Controller.java
+++ src/main/java/com/meluham/ems/controller/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/ms")
     public String loveyou(){
-        return "<h1>I Love You Sruthi</h1><br><h1>Happy ROSE DAY</h1>";
+        return "<h1>I Love You Sruthi</h1><br><h1>Happy ROSE DAY</h1><br><h1>love you mithuetta...</h1>";
     }
     
 }
